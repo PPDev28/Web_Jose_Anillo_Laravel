467d16c91b38538a7778c0708af050a55206d821
Pepe <ppdev28@gmail.com>
2024-04-21 18:52:05 +0200

    navbar js modified

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -2,12 +2,11 @@
     <nav class="navbar navbar-expand-lg navbar-light py-2 navbar-expand-md">
         <div class="container">
             <a href="{{ route('welcome') }}" class="navbar-brand px-lg-4 m-0 mr-auto">
-                <img src="{{asset('img/logoJoseAnilloBlanco.png')}}" width="200" height="100"
-                     alt="José Anillo">
+                <img src="{{asset('img/logoJoseAnilloBlanco.png')}}" width="200" height="100" alt="José Anillo">
             </a>
             <button type="button" class="navbar-toggler" id="menu-toggle">
                 <svg class="navbar-toggler-icon" xmlns="http://www.w3.org/2000/svg" width="23" height="15"
-                     viewBox="0 0 23 15" fill="none">
+                    viewBox="0 0 23 15" fill="none">
                     <path d="M23 1H0" stroke="white"></path>
                     <path d="M23 8H0" stroke="white"></path>
                     <path d="M23 14H0" stroke="white"></path>
@@ -17,26 +16,26 @@
                 <ul class="navbar-nav ml-auto" id="nav-links">
                     <li class="nav-item">
                         <a href="{{ route('welcome') }}"
-                           class="nav-link a-item {{ Route::currentRouteName() === 'welcome' ? 'active' : '' }}">INICIO</a>
+                            class="nav-link a-item {{ Route::currentRouteName() === 'welcome' ? 'active' : '' }}">INICIO</a>
                     </li>
                     <li class="nav-item">
                         <a href="{{ route('course.index') }}"
-                           class="nav-link a-item {{ Route::currentRouteName() === 'course.index' ? 'active' : '' }}">CURSOS</a>
+                            class="nav-link a-item {{ Route::currentRouteName() === 'course.index' ? 'active' : '' }}">CURSOS</a>
                     </li>
                     <li class="nav-item">
                         <a href="#" class="nav-link a-item">EVENTOS</a>
                     </li>
                     <li class="nav-item">
                         <a href="{{ route('gallery.index') }}"
-                           class="nav-link a-item {{ Route::currentRouteName() === 'gallery.index' ? 'active' : '' }}">GALERIA</a>
+                            class="nav-link a-item {{ Route::currentRouteName() === 'gallery.index' ? 'active' : '' }}">GALERIA</a>
                     </li>
                     <li class="nav-item">
                         <a href="{{ route('shop.index') }}"
-                           class="nav-link a-item {{ Route::currentRouteName() === 'shop.index' ? 'active' : '' }}">TIENDA</a>
+                            class="nav-link a-item {{ Route::currentRouteName() === 'shop.index' ? 'active' : '' }}">TIENDA</a>
                     </li>
                     <li class="nav-item">
                         <a href="{{ route('contact.index') }}"
-                           class="nav-link a-item {{ Route::currentRouteName() === 'contact.index' ? 'active' : '' }}">CONTACTO</a>
+                            class="nav-link a-item {{ Route::currentRouteName() === 'contact.index' ? 'active' : '' }}">CONTACTO</a>
                     </li>
                 </ul>
             </div>
@@ -48,9 +47,9 @@
 <script>
     $(document).ready(function () {
         $('#menu-toggle').click(function () {
-            $('#nav-links').slideToggle();
-            $('.ul-container').toggleClass('bg-transparent');
+            console.log("Validación de click");
+            $('.collapse').slideToggle();
+            // $('.ul-container').toggleClass('bg-transparent');
         });
     });
 </script>
-
