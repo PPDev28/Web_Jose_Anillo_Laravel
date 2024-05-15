33e8f6baec41708afd50ff9f1af19281c35f7141
JoseRamosLado <j.ramoslado2001@gmail.com>
2024-05-15 22:15:18 +0200

    change route to youtube

--- resources/views/footer2.blade.php
+++ resources/views/footer2.blade.php
@@ -8,7 +8,7 @@
                     <a class="btn btn-lg btn-link mr-2" href="https://www.facebook.com/joseanillosalazar/?locale=es_ES" target="_blank"><i class="fab fa-facebook-f"></i></a>
                     <a class="btn btn-lg btn-link mr-2" target="_blank" href="https://open.spotify.com/intl-es/artist/14jkAPjXRzlL89CjZedmhh"><i class="fa-brands fa-spotify"></i></a>
                     <a class="btn btn-lg btn-link mr-2" href="https://www.instagram.com/joseanillo.music/?hl=es" target="_blank"><i class="fab fa-instagram"></i></a>
-                    <a class="btn btn-lg btn-link" href="https://www.youtube.com/@JoseAnilloOficial" target="_blank"><i class="fa-brands fa-youtube"></i></a>
+                    <a class="btn btn-lg btn-link" href="https://www.youtube.com/c/Jos%C3%A9AnilloOficial" target="_blank"><i class="fa-brands fa-youtube"></i></a>
                 </div>
             </div>
             <div class="col-md-3 d-flex justify-content-end">
